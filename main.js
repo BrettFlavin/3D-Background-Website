c253871c15671805a66d8959cfa992a5e3602627
Brett Flavin <brettflavin@yahoo.com>
2020-11-16 22:31:40 -0800

    added features cards, added photos and created photo gallery section, changed theme colors

--- main.js
+++ main.js
@@ -3,10 +3,44 @@ const selectElement = (element) => {
     return document.querySelector(element);
 };
 
+/***********************************************************************/
+// toggle the nav-link open when the hamburger menu toggle is clicked
 let menuToggler = selectElement('.menu-toggle');
 let body = selectElement('body');
 
 menuToggler.addEventListener('click', () => {
     body.classList.toggle('open');
 });
+/***********************************************************************/
 
+/***********************************************************************/
+// SLIDESHOW
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("galleryImage");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+} 
+/***********************************************************************/
